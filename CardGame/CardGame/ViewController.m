7e243499880fccc4935617f09c3da995d835dd4b
frozenfung <frozenfung@gmail.com>
2015-03-08 12:03:22 +0800

    Build flip card function

--- CardGame/CardGame/ViewController.m
+++ CardGame/CardGame/ViewController.m
@@ -7,21 +7,27 @@
 //
 
 #import "ViewController.h"
+#import "PlayingCardDeck.h"
 
 @interface ViewController ()
-
+@property (strong, nonatomic) PlayingCardDeck *deck;
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-  [super viewDidLoad];
-  // Do any additional setup after loading the view, typically from a nib.
+- (PlayingCardDeck *)deck {
+  if(!_deck) _deck = [[PlayingCardDeck alloc] init];
+  return _deck;
 }
 
-- (void)didReceiveMemoryWarning {
-  [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
+- (IBAction)touchCardButton:(UIButton *)sender {
+  if ([sender.currentTitle length]){
+    [sender setBackgroundImage: [UIImage imageNamed: @"cardback"] forState:UIControlStateNormal];
+    [sender setTitle: @"" forState:UIControlStateNormal];
+  }else{
+    Card* card = [self.deck drawRandomCard];
+    [sender setBackgroundImage: [UIImage imageNamed: @"cardfront"] forState:UIControlStateNormal];
+    [sender setTitle: card.contents forState:UIControlStateNormal];
+  }
 }
-
 @end
