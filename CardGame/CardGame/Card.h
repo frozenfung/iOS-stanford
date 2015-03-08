381337c168ea8d7f9febed2669dd2f706d72038c
frozenfung <frozenfung@gmail.com>
2015-03-08 23:55:42 +0800

    Fix typo

--- CardGame/CardGame/Card.h
+++ CardGame/CardGame/Card.h
@@ -12,7 +12,7 @@
 @interface Card : NSObject
 @property (strong, nonatomic) NSString *contents;
 @property (nonatomic, getter=isChosen) BOOL chosen;
-@property (nonatomic, getter=isMatch) BOOL matched;
+@property (nonatomic, getter=isMatched) BOOL matched;
 
 - (int) match: (Card*) card;
 
